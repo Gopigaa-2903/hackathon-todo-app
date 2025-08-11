52b96dc2c58a784667d7f37c6d8daa316a44162d
Gopigaa-2903 <gopigaag583223104030@nprcolleges.org>
2025-08-11 13:45:51 +0530

    Added ShellCycle logo to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./styles.css";
+import logo from "./assets/logo.png"; // Import logo
+
 
 export default function App() {
   const [task, setTask] = useState("");
